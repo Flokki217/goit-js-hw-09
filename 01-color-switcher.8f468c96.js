790c2442517235cea1e5bb2b86bae1b3cb338212
Flokki217 <126098114+Flokki217@users.noreply.github.com>
2023-07-22 18:15:04 +0000

    Deploying to gh-pages from @ Flokki217/goit-js-hw-09@bb711928c7cbab78469955c8639776836cd996a7 🚀

--- 01-color-switcher.8f468c96.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");e.addEventListener("click",(()=>{changeColor=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),e.disabled=!0})),t.addEventListener("click",(()=>{clearInterval(changeColor),e.disabled=!1}));
-//# sourceMappingURL=01-color-switcher.8f468c96.js.map
